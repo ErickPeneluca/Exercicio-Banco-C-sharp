2e8b333edf4307f94fe0e795e399df2df0801860
Erick Peneluca <erickpeneluca@gmail.com>
2021-10-23 20:43:47 -0300

    Adicionar arquivos de projeto.

---
+++ Banco/Conta.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Banco
+{
+    class Conta
+    {
+        private int _numero;
+        private string _nome;
+        private double _saldo;
+
+        public Conta()
+        {
+        }
+
+        public string Nome
+        {
+            set
+            {
+                if (value != null && value.Length > 2)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public int Numero
+        {
+            set
+            {
+                _numero = value;
+            }
+        }
+
+        public double Saldo
+        {
+            set { _saldo = value; }
+        }
+
+        public void AdicionarDinheiro(double depositoInicial)
+        {
+            _saldo = depositoInicial + _saldo;
+        }
+
+        public void RetirarDinheiro(double saqueInicial)
+        {
+            _saldo = (_saldo - saqueInicial) - 5;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + _numero + ", Titular: " + _nome + ", Saldo: $ " + _saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Banco/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta();
+            Console.Write("Entre o número da conta: ");
+            conta.Numero = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Entre o titular da conta: ");
+            conta.Nome = Console.ReadLine();
+
+            Console.WriteLine("Haverá depósito inicial (s/n)?");
+            string resposta = Console.ReadLine();
+
+            if (resposta == "s" || resposta == "S")
+            {
+
+                Console.Write("Entre com o depósito inicial: ");
+                conta.Saldo = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Dados da conta: " + conta);
+
+
+                Console.WriteLine("Entre um valor para depósito: ");
+                conta.AdicionarDinheiro(double.Parse(Console.ReadLine()));
+
+                Console.WriteLine("Dados da conta atualizados: ");
+                Console.WriteLine(conta);
+
+                Console.WriteLine("Entre um valor para saque: ");
+                conta.RetirarDinheiro(double.Parse(Console.ReadLine()));
+
+                Console.WriteLine("Dados da conta atualizados: ");
+                Console.WriteLine(conta);
+            }
+            else if (resposta == "n" || resposta == "N")
+            {
+
+                Console.WriteLine("Dados da conta: " + conta);
+
+                Console.WriteLine("Entre um valor para depósito: ");
+                conta.AdicionarDinheiro(double.Parse(Console.ReadLine()));
+
+                Console.WriteLine("Dados da conta atualizados: ");
+                Console.WriteLine(conta);
+
+                Console.WriteLine("Entre um valor para saque: ");
+                conta.RetirarDinheiro(double.Parse(Console.ReadLine()));
+
+                Console.WriteLine("Dados da conta atualizados: ");
+                Console.WriteLine(conta);
+
+            }
+            else
+            {
+                Console.WriteLine("Escolha uma opção válida por favor!");
+            }
+
+        }
+    }
+}
